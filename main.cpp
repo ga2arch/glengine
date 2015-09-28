6790325f5fae34698292dc2f2b6f0552c98dae36
ga2arch <gabriele.carrettoni@gmail.com>
2015-09-28 21:40:47 +0200

    added dot/cross product and tests

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
-#define LOOP1(a) void p(std::string s) { std::cout << s << std::endl; }
-#define LOOP2(a) void b(std::string s) { std::cout << s << std::endl; } LOOP1(a)
-#define LOOP3(a) void xy() a LOOP2(a)
-
-#define LOOPN(n,a) LOOP##n(a)
-
 #include <iostream>
 
-LOOPN(3, {
-    std::cout << "ciao" << std::endl;
-});
 
 int main(void) {
-    xy();
+    
 }
 
